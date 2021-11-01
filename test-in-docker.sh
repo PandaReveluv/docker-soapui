4b7debb9a789ab26754b4b5033a76f55dfebb6a0
PandaReveluv <dat19071998@gmail.com>
2021-11-01 14:48:41 +0700

    update env vả

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -3,7 +3,7 @@ set -e -v
 docker-compose down
 docker-compose pull
 docker-compose build
-if docker-compose run --service-ports --rm tests bash ./test.sh; then
+if docker-compose run -e urlPath=${urlPath} --service-ports --rm tests bash ./test.sh; then
   echo "All test passed successfully. Shut down containers......"
   docker-compose down
   exit 0
