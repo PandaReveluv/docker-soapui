4a0ad421ad66325f7d9711ee93e53d6fe03ee441
PandaReveluv <62783730+PandaReveluv@users.noreply.github.com>
2021-11-01 15:50:21 +0700

    Update test-in-docker.sh
    update sh

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -5,7 +5,7 @@ echo "URL path = ${urlPath}"
 docker-compose down
 docker-compose pull
 docker-compose build -q
-if docker-compose run -e urlPath="${urlPath}" --service-ports --rm tests bash ./test.sh; then
+if docker-compose run -e urlPath=${urlPath} --service-ports --rm tests bash ./test.sh; then
   echo "All test passed successfully. Shut down containers......"
   docker-compose down
   exit 0
