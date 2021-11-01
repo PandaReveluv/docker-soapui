4d25b59803eed39e393ddc63b71dc04fc8ddd46b
PandaReveluv <dat19071998@gmail.com>
2021-11-01 14:36:12 +0700

    update code

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 set -e -v
 
-./SoapUI-5.6.0/bin/testrunner.sh -r /test/demo-soapUI-soapui-project.xml -GurlPath=${urlPath}
+./SoapUI-5.6.0/bin/testrunner.sh -r /test/demo-soapUI-soapui-project.xml -GurlPath=$urlPath
