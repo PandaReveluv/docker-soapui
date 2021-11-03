011f4364738f400e785117225b4f86a0a1bc9ff0
PandaReveluv <dat19071998@gmail.com>
2021-11-03 17:04:10 +0700

    update sh

--- bin/test-in-docker.sh
+++ bin/test-in-docker.sh
@@ -1,4 +1,4 @@
 set -e 
 set -x
 
-./SoapUI-5.6.0/bin/testrunner.sh -r /test/demo-soapUI-soapui-project.xml -GurlPath=${urlPath}
+bash ./SoapUI-5.6.0/bin/testrunner.sh -r /test/demo-soapUI-soapui-project.xml -GurlPath=${urlPath}
--- bin/test.sh
+++ bin/test.sh
@@ -4,7 +4,7 @@ set -x
 docker-compose -f docker/docker-compose.yml down
 docker-compose -f docker/docker-compose.yml pull
 docker-compose -f docker/docker-compose.yml build
-if docker-compose -f docker/docker-compose.yml run -e urlPath=${urlPath} --service-ports --rm tests bash ./bin/test-in-docker.sh; then
+if docker-compose -f docker/docker-compose.yml run -e urlPath=${urlPath} --service-ports --rm tests bash ./test-in-docker.sh; then
   echo "All test passed successfully. Shut down containers......"
   docker-compose -f docker/docker-compose.yml down
   exit 0
